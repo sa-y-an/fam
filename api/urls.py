54dc3a2f2bf9f268d329effe902120c1e79a91c6
sa-y-an <sayan.ee.nitd@gmail.com>
2022-04-07 17:00:26 +0530

    changed to default router

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from .views import ParentList, ChildrenList
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 app_name = 'api'
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register('parent', ParentList, basename='parent')
 router.register('child', ChildrenList, basename='child')
 urlpatterns = router.urls
